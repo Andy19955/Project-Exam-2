8067b7bece601ae9c1e2e957f9124eb71332c31b
Andreas Thune <andy_19955@hotmail.com>
2026-06-23 01:05:14 +0200

    Create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Footer() {
+  return (
+    <footer className="w-full h-16 shadow-md flex items-center justify-center">
+      <p className="text-gray-600">© 2026 Holidaze. All rights reserved.</p>
+    </footer>
+  );
+}
