72d71fbc249c7ebd7be2c539d91e521acbf01dc2
Andreas Thune <andy_19955@hotmail.com>
2026-07-05 02:11:44 +0200

    Fix header centering

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
   return (
     <header className="w-full flex items-center justify-between px-6 py-4 bg-white border-b border-(--border) shadow-md min-h-20">
       <MobileMenu open={open} onClose={() => setOpen(false)} isLoggedIn={Boolean(user)} onLogout={logout} name={String(name)} />
-      <div className="flex items-center justify-between max-w-7xl w-full">
+      <div className="flex items-center justify-between max-w-7xl w-full mx-auto">
         <Link href="/" className="text-xl font-bold">
           Holidaze
         </Link>
