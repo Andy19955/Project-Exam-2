a59bfaa7960b424a23fb1725d94ece86832092a4
Andreas Thune <andy_19955@hotmail.com>
2026-07-05 03:27:35 +0200

    Update profile url to use authstore

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,19 +3,16 @@
 import Link from "next/link";
 import { useState } from "react";
 import MobileMenu from "./MobileMenu";
-// import { useAuth } from "@/context/AuthContext";
 import { useAuthStore } from "@/store/authStore";
-import loadLocalStorage from "@/api/helpers/loadLocalStorage";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
   const user = useAuthStore((state) => state.user);
   const logout = useAuthStore((state) => state.clearAuth);
-  const name = loadLocalStorage("name");
 
   return (
     <header className="w-full flex items-center justify-between px-6 py-4 bg-white border-b border-(--border) shadow-md min-h-20">
-      <MobileMenu open={open} onClose={() => setOpen(false)} isLoggedIn={Boolean(user)} onLogout={logout} name={String(name)} />
+      <MobileMenu open={open} onClose={() => setOpen(false)} isLoggedIn={Boolean(user)} onLogout={logout} name={String(user?.name)} />
       <div className="flex items-center justify-between max-w-7xl w-full mx-auto">
         <Link href="/" className="text-xl font-bold">
           Holidaze
@@ -23,7 +20,7 @@ export default function Header() {
         <nav className="hidden md:flex items-center gap-3">
           {user ? (
             <>
-              <Link href={`/profiles/${name}`} className="hover:text-(--text-secondary) hover:underline transition-colors duration-200">
+              <Link href={`/profiles/${user?.name}`} className="hover:text-(--text-secondary) hover:underline transition-colors duration-200">
                 Profile
               </Link>
               <button onClick={logout} className="cursor-pointer hover:text-(--text-secondary) hover:underline transition-colors duration-200">
