b21fedb28b5c800db4a6ab5639f297c6feb25ac4
Andreas Thune <andy_19955@hotmail.com>
2026-07-06 01:53:49 +0200

    Move page specific component to page folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Hero from "@/components/landing/Hero";
-import Venues from "@/components/landing/Venues";
+import Hero from "@/app/components/Hero";
+import Venues from "@/app/components/Venues";
 
 export default function Home() {
   return (
