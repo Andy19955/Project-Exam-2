7a58e8bfc73e5cf39accfa80223d019f18ea4c54
Andreas Thune <andy_19955@hotmail.com>
2026-07-28 01:57:48 +0200

    Add predefined text color and remove background color bug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <footer className="w-full h-16 shadow-md flex items-center justify-center">
-      <p className="bg-(--text-secondary)">© 2026 Holidaze. All rights reserved.</p>
+      <p className="text-(--text-secondary)">© 2026 Holidaze. All rights reserved.</p>
     </footer>
   );
 }
