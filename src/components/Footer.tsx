b1d7412b2a711f4ea170aa4708da79b66ee4623d
Andreas Thune <andy_19955@hotmail.com>
2026-07-28 01:56:02 +0200

    Change to predefined color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <footer className="w-full h-16 shadow-md flex items-center justify-center">
-      <p className="text-gray-600">© 2026 Holidaze. All rights reserved.</p>
+      <p className="bg-(--text-secondary)">© 2026 Holidaze. All rights reserved.</p>
     </footer>
   );
 }
