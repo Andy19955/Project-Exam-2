34db800b81d093d4e094541aa0ec6c7fd82e55c8
Andreas Thune <andy_19955@hotmail.com>
2026-08-05 02:03:10 +0200

    Add logout handler function for redirecting to landing page after logging out

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,22 @@ import Image from "next/image";
 import { useState } from "react";
 import MobileMenu from "./MobileMenu";
 import { useAuthStore } from "@/store/authStore";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
   const user = useAuthStore((state) => state.user);
+  const router = useRouter();
   const logout = useAuthStore((state) => state.clearAuth);
+
+  function handleLogout() {
+    logout();
+    router.push("/");
+  }
 
   return (
     <header className="w-full flex items-center justify-between px-6 py-4 bg-white border-b border-(--border) shadow-md min-h-20">
-      <MobileMenu open={open} onClose={() => setOpen(false)} isLoggedIn={Boolean(user)} onLogout={logout} />
+      <MobileMenu open={open} onClose={() => setOpen(false)} isLoggedIn={Boolean(user)} onLogout={handleLogout} />
       <div className="flex items-center justify-between max-w-7xl w-full mx-auto">
         <Link href="/" className="text-xl font-bold flex items-center gap-2">
           <Image src="/images/icon-logo.png" alt="Holidaze" width={45} height={45} />
@@ -25,7 +32,7 @@ export default function Header() {
               <Link href="/profile" className="hover:text-(--text-secondary) hover:underline transition-colors duration-200">
                 Profile
               </Link>
-              <button onClick={logout} className="cursor-pointer hover:text-(--text-secondary) hover:underline transition-colors duration-200">
+              <button onClick={handleLogout} className="cursor-pointer hover:text-(--text-secondary) hover:underline transition-colors duration-200">
                 Logout
               </button>
             </>
